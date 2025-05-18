cba0d210a50b36fe1be264ff8c5d1fa94b77953a
David A. Ramírez Luna <davidrrluna@gmail.com>
2025-05-18 18:35:04 -0400

    first commit

---
+++ src/main/java/com/edutech/msclases/MsclasesApplication.java
@@ -0,0 +1,13 @@
+package com.edutech.msclases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsclasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsclasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edutech/msclases/MsclasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutech.msclases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsclasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
